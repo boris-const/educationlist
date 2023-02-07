d4c7726d6784785ec441e81b5fa11010274f1b00
Boris <abdul662isaev@gmail.com>
2023-02-07 21:43:09 +0300

    commit

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
+import { ITodo } from "../interfaces/todolist";
 
-export const TodoList: React.FC = () => {    
-    return (
-        <h1>Helloq world</h1>        
-    )
+export const TodoList: React.FC = () => {
+  const todoArr: ITodo[] = [
+    {
+      title: "Go to gym",
+      description: "You need to be realy powerful !",
+      isDone: false,
+    },
+    {
+      title: "Go to kitchen",
+      description: "You need to be healfy !",
+      isDone: false,
+    },
+    {
+      title: "Go to badroom",
+      description: "You need to be fine !",
+      isDone: false,
+    },
+  ];
+
+  return (
+    <>
+      <div className="todo_list">
+        <ol>
+          {todoArr.map((el, idx) => (
+            <li
+              key={idx}
+            >{`Activity name: ${el.title}, Activity description: ${el.description}, isDone: ${el.isDone}`}</li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
 };
